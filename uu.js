76ce587fdea9fbf73a98ab656c6bde7300281023
bolot0 <kamcybek1957@gmail.com>
2022-10-06 15:44:24 +0600

    dz1

---
+++ uu.js
@@ -0,0 +1,84 @@
+// // var hexWrapper = document.getElementById("hexWrapper");
+
+// // var rgbWrapper = document.getElementById("rgbWrapper");
+
+// // var hexColor=document.getElementById("hexColor");
+
+// // var rgbColor = document.getElementById("rgbColor");
+
+// // console.log(rgbWrapper);
+
+// // hexPage.addEventListener("click", () => {
+
+// //     if (hexPage.style.textDecoration != "underline") {
+
+// //         hexWrapper.style.display = "block";
+
+// //         rgbWrapper.style.display = "none";
+
+// //         hexPage.style.textDecoration = "underline";
+// //         rgbPage.style.textDecoration
+// //         "none";
+// //     }
+// //     })
+// var changeBackGround = document.body
+
+// var hexPage = document.getElementById("hexPage");
+// var rgbPage = document.getElementById("rgbPage");
+
+// var hexWrapper = document.getElementById("hexWrapper");
+// var rgbWrapper = document.getElementById("rgbWrapper");
+
+// var hexColor = document.getElementById("hexColor");
+// var rgbColor = document.getElementById("rgbColor");
+
+// console.log(rgbWrapper);
+
+// hexPage.addEventListener("click", () => {
+
+//     if (hexPage.style.textDecoration != "underline") {
+//         hexWrapper.style.display = "block";
+//         rgbWrapper.style.display = "none";
+
+//         hexPage.style.textDecoration ="underline";
+//         rgbColor.style.textDecoration = "none";
+
+//     }
+// })
+// hexPage.addEventListener("click", () => {
+
+//     if (rgbPage.style.textDecoration != "underline") {
+//         rgbWrapper.style.display = "block";
+//         hexWrapper.style.display = "none";
+
+//         rgbPage.style.textDecoration ="underline";
+//         hexPage.style.textDecoration = "none";
+
+//     }
+// })
+// var hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c","d","e","f",]
+
+//     console.log(hex[11]);
+
+// var randomNumberValue;
+
+// hexColor.addEventListener("click", () => 
+// var hexColorValue = "#";
+
+// for (var i = 0; i < 6; i++) {
+// randomNumberValue = Math.floor(Math.random() * hex.length); hexColorValue+hex[randomNumberValue];} 
+// hexColor.textContent = hexColorValue;
+
+// hexColorValue+hex[randomNumberValue];
+
+// changeBackGround;
+// })
+const arr = prompt ('зеленый желтый красный');
+
+   if (arr === 'зеленый') {
+      console.log('идти');
+   } else if (arr === 'желтый') {
+      console.log('жди');
+   } else if (arr === 'красный') {
+      console.log('стой');
+  }
